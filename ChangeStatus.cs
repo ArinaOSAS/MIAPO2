d6255f8ae930267ced606954b961cd8d32bbe5db
ArinaOSAS <gr604_koaan@ttit.local>
2023-05-17 14:28:45 +0700

    Добавление функции Смена статуса

---
+++ ChangeStatus.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Films._1;
+
+public static class ChangeStatus
+{
+    public static List<Movie> ChangeStatusYes(Movie selectFilm)
+    {
+        if (selectFilm.Fkstatus == 2)
+        {
+            new DBFilmsContext().Movies.First(a => a.Idmovie == selectFilm.Idmovie).Fkstatus = 1;
+            new DBFilmsContext().SaveChanges();   
+        }
+        return new DBFilmsContext().Movies.ToList();
+    }
+
+}
